ffce4fe7b597681807dfcdd254cb1443393eef6c
bitfrost7 <15591017398@163.com>
2026-07-14 20:42:16 +0800

    fix: add titlebar offset to workflows view

--- apps/desktop/src/app/workflows/index.tsx
+++ apps/desktop/src/app/workflows/index.tsx
@@ -1,8 +1,14 @@
 import { App } from '../workflows-editor/App'
+import { cn } from '@/lib/utils'
 
 export function WorkflowsView() {
   return (
-    <div className="flex h-full w-full flex-1 overflow-hidden">
+    <div className={cn(
+      'flex h-full w-full flex-1 flex-col overflow-hidden',
+      // Offset the SPA's negative margins and clear the titlebar
+      '[&_.hw-root]:!m-0 [&_.hw-root]:!h-full',
+      'pt-(--titlebar-height)'
+    )}>
       <App />
     </div>
   )
